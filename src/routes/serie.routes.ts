63fd3c057f0ef825b41448fc035ac9af6c8da692
EdMorales2986 <ed.morales6892@gmail.com>
2023-12-07 23:16:09 -0400

    forgot about the routes

--- src/routes/serie.routes.ts
+++ src/routes/serie.routes.ts
@@ -1,9 +1,20 @@
 import { Router } from "express";
-import { searchSerie, getSeries } from "../controllers/series.controllers";
+import {
+  searchSerie,
+  getSeries,
+  filterByYear,
+  filterByGenre,
+  newestFirst,
+  oldestFirst,
+} from "../controllers/series.controllers";
 
 const router = Router();
 
 router.get("/series", getSeries);
 router.post("/series/search", searchSerie);
+router.get("/series/filter/year/:startDate/:endDate", filterByYear); // 2020 - 2023
+router.get("/series/filter/genre/:genre", filterByGenre); // genre must start with uppercase: "Action & Fantasy"
+router.get("/series/newest", newestFirst);
+router.get("/series/oldest", oldestFirst);
 
 export default router;
